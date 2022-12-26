d28798395dce49dbf0fa38025054d54c0234eae5
shahraim <shahraim.khan27112002@gmail.com>
2022-12-26 11:34:13 +0500

    this is for practice add git commit

---
+++ index.js
@@ -0,0 +1,121 @@
+
+
+//commit anything with slash
+
+                                // class 01
+
+console.clear();
+
+// console.log(10 + 10);
+// console.log(10 * 10);
+// console.log(10, 10);
+// console.log("this is world");                 //"" for showing alphabet
+// console.log("helo", 5 * 10 + 5);
+
+// console.warn("this is warning");
+
+// console.error("error in ur pc");
+
+// document.write("hi and by");
+// document.write("hi and by","<br>");              //for breal line
+// document.write("<i>","<b>","hi and by","</b>","</i>","<br>");       //bold and italic
+// document.write("additional=",10+5);
+
+// alert("this is alert");
+// window.alert("same like a alert");
+
+// window.confirm("this is a confrim box");
+
+            // variables
+// a=5
+// b=10
+// console.log(a+b);
+
+// num1 = 20; 
+// num2 = 30;
+// add = (num1+num2);
+
+// console.log(add);
+
+// a;
+// var a=10;
+// let b=20;
+
+
+
+
+
+                                    // class 02
+ 
+           // primitive data
+
+// number 
+
+// let a=10;
+// b=typeof(a);
+// document.write(b);
+
+// String
+
+// let a="20";
+// b=typeof(a);
+// document.write(b);
+
+// Boolean
+
+// let a=true;      //true and afasle
+// b=typeof(a);
+// document.write(b);
+
+// undefined
+
+// let a;     
+// b=typeof(a);
+// document.write(b);
+
+// null but show oobject
+
+// let a=null;     
+// b=typeof(a);
+// document.write(b);
+
+              // Reference data type
+
+    //   Array
+
+    // let car=['BMW','BUGATTI','FERRARI','ODDY','LAMBORGHINI'];
+    // document.write(car);
+
+    // let car=['BMW','BUGATTI','FERRARI','ODDY','LAMBORGHINI'];
+    // document.write(car[2]);
+
+    // let car=['BMW','BUGATTI','FERRARI','ODDY','LAMBORGHINI'];
+    // console.log(car);
+
+    // let person=['shah',19,null,true,undefined];
+    // document.write(person);
+
+            // prompt
+
+    // prompt('enter your name');
+
+    // let a=prompt('enter ur papa ka naam');
+    // document.write(a);
+
+                // task user promopt & Array
+
+    // let car=['BMW','BUGATTI','FERRARI','ODDY','LAMBORGHINI'];
+    // document.write(car[prompt('khan bhai likh')]);   
+    
+    // let car=[prompt("enter name"),prompt("enter name"),prompt("enter name"),prompt("enter name"),prompt("enter name")];
+    // document.write(car);   
+
+
+                // Object
+    let bike={}
+
+
+
+ 
+
+
